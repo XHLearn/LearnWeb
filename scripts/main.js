216fd4e7f0042e831925ab8f07a63c0578eff551
xiaohao <hao.xiao@narwal.com>
2024-06-13 18:08:18 +0800

    js basics

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+
+
+
+let myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
+
+
+d = RobotDevice(
+    product_id=device.product_id,
+    device_id=device.device_id,
+    env=device.env.value,
+    type=device.type.value,
+    description=device.description,
+)
