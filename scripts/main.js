417f6766fa43ab6c49cae48710b84f0b192afa83
xiaohao <hao.xiao@narwal.com>
2024-06-14 17:11:29 +0800

    change img

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,16 @@
 
 
-let myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
+let myImg = document.querySelector("img");
 
+myImg.onclick = () => {
+    let mySrc = myImg.getAttribute("src");
+    if (mySrc === "../images/logo.png") {
+        myImg.setAttribute("src", "../images/logo2.png");
+    } else {
+        myImg.setAttribute("src", "../images/logo.png");
+    }
+};
 
-document.querySelector("html").addEventListener("click", () => {
-    alert("别戳我，我怕疼。");
-});
+// document.querySelector("html").addEventListener("click", () => {
+//     alert("别戳我，我怕疼。");
+// });
