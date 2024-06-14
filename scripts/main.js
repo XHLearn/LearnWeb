ef4056e3910c44cd23531c758eda6b1dc350eaa4
xiaohao <hao.xiao@narwal.com>
2024-06-14 17:17:43 +0800

    切换用户

--- scripts/main.js
+++ scripts/main.js
@@ -3,13 +3,33 @@
 let myImg = document.querySelector("img");
 
 myImg.onclick = () => {
-    let mySrc = myImg.getAttribute("src");
-    if (mySrc === "../images/logo.png") {
-        myImg.setAttribute("src", "../images/logo2.png");
-    } else {
-        myImg.setAttribute("src", "../images/logo.png");
-    }
+  let mySrc = myImg.getAttribute("src");
+  if (mySrc === "../images/logo.png") {
+    myImg.setAttribute("src", "../images/logo2.png");
+  } else {
+    myImg.setAttribute("src", "../images/logo.png");
+  }
 };
+
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1")
+function setUserName() {
+  let myName = prompt("请输入你的名字。");
+  localStorage.setItem("name", myName);
+  myHeading.textContent = "Mozilla 酷毙了，" + myName;
+}
+
+if (localStorage.getItem("name")) {
+  let storedName = localStorage.getItem("name");
+  myHeading.textContent = "Mozilla 酷毙了，" + storedName;
+} else {
+  setUserName();
+}
+
+myButton.onclick = () => {
+  setUserName();
+}
 
 // document.querySelector("html").addEventListener("click", () => {
 //     alert("别戳我，我怕疼。");
