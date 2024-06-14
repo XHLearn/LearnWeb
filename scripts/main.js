e53540c209dc8f759b9b0131c2d25ece88908101
xiaohao <hao.xiao@narwal.com>
2024-06-14 17:07:49 +0800

    alert

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,9 @@
 
 
-
 let myHeading = document.querySelector("h1");
 myHeading.textContent = "Hello world!";
 
 
-d = RobotDevice(
-    product_id=device.product_id,
-    device_id=device.device_id,
-    env=device.env.value,
-    type=device.type.value,
-    description=device.description,
-)
+document.querySelector("html").addEventListener("click", () => {
+    alert("别戳我，我怕疼。");
+});
